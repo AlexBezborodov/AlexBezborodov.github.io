f7011a690e94063f37af6e8b7a3c57fe73eb8253
Alex Bezborodov <44156457+AlexBezborodov@users.noreply.github.com>
2020-04-24 00:40:37 +0300

    Add files via upload

---
+++ precache-manifest.24e9d116ff80ffe77eddcb3f6ba48c12.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e9949d4330ce6ca68b2cd888aea7372",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21d60b9fac79c5b9e091",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "8b472958eabc8998ee23",
+    "url": "/static/css/main.1830aea8.chunk.css"
+  },
+  {
+    "revision": "21d60b9fac79c5b9e091",
+    "url": "/static/js/2.a813a5cd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.a813a5cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b472958eabc8998ee23",
+    "url": "/static/js/main.66a450d6.chunk.js"
+  },
+  {
+    "revision": "c2e4d48f121e54de8af4",
+    "url": "/static/js/runtime-main.c6d65b3c.js"
+  },
+  {
+    "revision": "20ed2b517c336f85556d6150ae04a12f",
+    "url": "/static/media/img-1.20ed2b51.jpg"
+  },
+  {
+    "revision": "0988a78961f1ed4d65a53b94f5391b09",
+    "url": "/static/media/img-2.0988a789.jpg"
+  },
+  {
+    "revision": "91f1309d2a4128c582aea9e60ded64b1",
+    "url": "/static/media/img-3.91f1309d.jpg"
+  }
+]);
